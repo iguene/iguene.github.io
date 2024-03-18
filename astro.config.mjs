0d633f6e8dcc61e3d79242f7fd06c51a44d6a106
iguene <118843629+iguene@users.noreply.github.com>
2024-03-18 23:13:40 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config'
 
+import netlify from "@astrojs/netlify/functions";
+
 export default defineConfig({
   site: 'https://iguene.github.io/',
   base: '/my-repo',
